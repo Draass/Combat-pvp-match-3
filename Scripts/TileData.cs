991edaada26bef193d14d841b66d45f44406f56d
Draass <94137985+Draass@users.noreply.github.com>
2022-05-07 02:15:09 +0300

    Create TileData.cs

---
+++ Scripts/TileData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TileData", menuName = "ScriptableObjects/TileDataScriptableObject", order = 1)]
+public class TileData : ScriptableObject
+{
+    public Sprite sprite;
+
+    public int manaValue;
+
+
+}
