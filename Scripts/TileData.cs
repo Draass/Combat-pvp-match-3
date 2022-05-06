32f554d04a694863bd594dff553223f17ac0f81c
Draass <94137985+Draass@users.noreply.github.com>
2022-05-07 02:19:26 +0300

    Update TileData.cs

--- Scripts/TileData.cs
+++ Scripts/TileData.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "TileData", menuName = "ScriptableObjects/TileDataScriptableObject", order = 1)]
+[CreateAssetMenu(fileName = "TileData", menuName = "ScriptableObjects/TileData", order = 1)]
 public class TileData : ScriptableObject
 {
     public Sprite sprite;
 
     public int manaValue;
-
-
 }
